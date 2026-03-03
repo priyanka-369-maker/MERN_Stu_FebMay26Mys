87072cfc855e037f951312e29f3a4bddaaf2a826
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-03-03 12:33:45 +0530

    Conditional, switch statements and Type coversion

---
+++ W3/D2/p4.js
@@ -0,0 +1,32 @@
+// Conditonal statements
+// let age = 20;
+// if(age <13){
+//     console.log("Chlid");
+// }
+// else if(age<18){
+//     console.log("Teenagar");
+// }
+// else{
+//     console.log("Adult");
+// }
+
+//Switch statements
+// console.log("Conditional statements");
+// let day = "tuesday";
+// switch (day) {
+//     case "monday" :
+//         console.log("start of the week");
+//         break;
+//         case "wednesday":
+//         console.log("mid of the week");
+//         break;
+//         case "friday":
+//         console.log("End of work week");
+//         break;
+//         default:
+//         console.log("some day in the week");
+//         break;
+// }
+// Type conversion
+let n = Number("123");
+console.log( "n: ",n, "Type of n:",typeof n ,"isNaN",isNaN(n));
