a5d3d4aa4cbfedc3d6c5b841a3469e9d23716c58
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-03-03 12:32:18 +0530

    Comparision operators

---
+++ W3/D2/p2.js
@@ -0,0 +1,11 @@
+// comparision operators
+console.log("Comparison operators");
+let x = 3, y = 3;
+console.log("x==y: ",x==y); //Loose equality
+console.log("x===y: ",x===y); //strict equality
+console.log("x!=y: ",x!=y); //Loose inequality
+console.log("x!==y: ",x!==y); //strict inequality
+console.log("x>3: " ,x>3); 
+console.log("x<3: ", x<3); 
+console.log("x>=3: ", x>=3);
+console.log("x<=3: ", x<=3); 
