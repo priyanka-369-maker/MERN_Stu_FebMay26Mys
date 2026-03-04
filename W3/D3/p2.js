f30c03013ff545385efd41c5eb4736a63efee8f7
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-03-04 12:40:27 +0530

    While/Do while

---
+++ W3/D3/p2.js
@@ -0,0 +1,15 @@
+//While loop
+console.log("while loop");
+
+let count = 0;
+while(count<5){
+    console.log("count:",count);
+    count++;
+}
+
+//do while
+let value = 0;
+do{
+    console.log("Value :",value);
+    value++;
+}while(value < 3);
