9a996a70f0e554eec365af37b25c50b02a9cf8e1
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-03-04 14:26:17 +0530

    Nested object & method

---
+++ W3/D3/p6.js
@@ -0,0 +1,17 @@
+//Nested object and method
+const student = {
+    firstname: "Priya",
+    lastname: "shivaram",
+    scores:{
+        math: 80,
+        science:83
+    },
+    hobbies:["reding","singing"],
+    fullname: function(){
+        return this.firstname + " "+this.lastname;
+    },
+    greet(){
+        console.log("Hi, ", this.fullname());
+    }
+};
+    console.log(student.fullname());
