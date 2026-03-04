3cd051832ded16c7e9ad8a7ca6d9449b9b925c2d
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-03-04 14:25:45 +0530

    Basics of objects

--- W3/D3/p5.js
+++ W3/D3/p5.js
@@ -1,9 +1,29 @@
-//Basics of objects
+// Basics of objects
 const person = {
-    name:"Rahul",
-    age:30,
+    name: "Rahul",
+    age: 30,
     isStudent: false
 };
-console.log("person",person);
-console.log("Name: ",person.name);
-console.log("age: ",person["age"]);
+
+console.log("person", person);
+console.log("Name: ", person.name);
+console.log("age: ", person["age"]);
+
+// Add a new property
+person.city = "Mysore";
+console.log("Person", person);
+
+// Modify
+person.age = 31;
+
+// Delete
+delete person.isStudent;
+console.log("delete", person);
+
+// Object constructor
+const car = new Object();
+car.make = "Audi";
+car.model = "A4";
+car.year = 2026;
+
+console.log(car);
