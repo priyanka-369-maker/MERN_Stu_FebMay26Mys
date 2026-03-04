55826a124e14a5adf1223b6a6125db072d8954c2
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-03-04 12:44:33 +0530

    For/Nested loop

--- W3/D3/p1.js
+++ W3/D3/p1.js
@@ -8,6 +8,7 @@ console.log("for loop examples");
 for(let i=5;i>0;i--){
     console.log("i:",i);//backword loop
 }
+//nested
 for(let i=1;i<=3;i++){
     for(let j=1;j<=2;j++){
         console.log(`i=${i},j=${j}`);
