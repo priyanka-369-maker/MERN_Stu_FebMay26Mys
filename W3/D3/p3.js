e04fd7b269abf6a888cb99790412ef5b806d349a
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-03-04 12:40:48 +0530

    Break/Continue

---
+++ W3/D3/p3.js
@@ -0,0 +1,15 @@
+// Break
+for(let i=0;i<5;i++){
+    if(i===2){
+     console.log("i",i);
+     break;
+    }
+}
+//Continue
+for(let i=0;i<5;i++){
+    if(i===2){
+     console.log("i",i);
+     continue;
+    }
+    
+}
