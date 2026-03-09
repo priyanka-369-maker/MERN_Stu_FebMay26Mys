3c0055f667a189d524e70956a1e458a8e2df5947
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-03-09 09:01:09 +0530

    this

---
+++ W3/D5/p11.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("compareBtn");
+btn.addEventListener("click",function(e){
+    console.log("Regular function");
+    console.log("this === btn",this===btn);
+    console.log("e current target === btn",e.currenttarget===btn);
+    console.log("e  target === btn",e.target===btn);
+});
+btn.addEventListener("click",(e) =>{
+    console.log("arrow function");
+    console.log("this === btn", this===btn);
+    console.log("e current target === btn",e.currenttarget===btn);
+    console.log("e  target === btn",e.target===btn);
+});
