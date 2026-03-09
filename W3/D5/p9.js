fd385243128f34fa40f44729242aca5830205047
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-03-09 08:57:00 +0530

    Advanced event Pattern

---
+++ W3/D5/p9.js
@@ -0,0 +1,12 @@
+const onceBtn = document.getElementById("onceBtn");
+//Limiting listener to once only for a event
+onceBtn.addEventListener("click",function(){
+    console.log("this click listener works only once.");
+},{once:true});
+//Global keyboard shortcut creation
+document.addEventListener("keydown",function(event){
+if(event.ctrlKey && event.key.toLowerCase()==="s"){
+    event.preventDefault();
+    console.log("Custom ctrl+s shortcut triggerd");
+}
+});
