a81df044dc9ded297df69aabd570bc82159aa395
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-03-16 16:26:51 +0530

    setTimeout

---
+++ W5/D1/P3.js
@@ -0,0 +1,7 @@
+console.log("Node JS architecture demo");
+console.log("1. Script started");
+//SetTimeOut
+setTimeout(()=>{
+    console.log("3. timer callback finished after waiting");
+},1000);
+console.log("2. Script continued without waiting for timer callback");
