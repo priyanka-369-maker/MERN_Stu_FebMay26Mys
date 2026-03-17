7b3b24dc09b4791b4fe2d83790df7f6113430c8a
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-03-17 17:02:38 +0530

    Portfolio project

---
+++ W4/D5/portfolio-project/js/data/projects.js
@@ -0,0 +1,48 @@
+const projectsData = [
+  {
+    id: 1,
+    name: "Smart companion app for traffic compliance",
+    category: "Python",
+    description:
+      "App which minimize road accident notify traffic rules .",
+    technologies: ["ReactJS", "ExpressJS", "MongoDB"],
+    status: "Live",
+    liveDemo: "#",
+    github: "#"
+  },
+
+
+  {
+    id: 2,
+    name: "Gola",
+    category: "MERN",
+    description:
+      "Cab booking platform that helps users quickly book rides for daily travel.",
+    technologies: ["ReactJS", "ExpressJS", "MongoDB"],
+    status: "Live",
+    liveDemo: "#",
+    github: "#"
+  },
+  {
+    id: 3,
+    name: "Portfolio Website",
+    category: "Frontend",
+    description:
+      "Responsive personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "Tailwind CSS", "JavaScript"],
+    status: "Live",
+    liveDemo: "#",
+    github: "#"
+  },
+  {
+    id: 4,
+    name: "Developer Stats Dashboard",
+    category: "JavaScript",
+    description:
+      "Dynamic dashboard that loads developer statistics and profile insights using Fetch API.",
+    technologies: ["JavaScript", "Fetch API", "JSON"],
+    status: "Demo",
+    liveDemo: "#",
+    github: "#"
+  }
+];
