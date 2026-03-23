fb86c6a580fb0f9b7aadafbc954b63c9a564be0d
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-03-23 16:51:13 +0530

    Import ES modules

---
+++ W6/D1/P6.mjs
@@ -0,0 +1,8 @@
+//Import ES modules
+import createInvoiceLabel,{calculateTotal,taxrate} from"./P5.mjs"
+const subtotal = 40000;
+const total = calculateTotal(subtotal);
+const label = createInvoiceLabel("INV-2026-001");
+console.log("Invoice Label:", label);
+console.log("Tax rate: ",taxrate);
+console.log("Final total: ",total);
