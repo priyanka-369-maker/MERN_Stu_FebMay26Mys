a8cfc6f02aae36c7c4c174dddb8a1052d758ec8b
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-03-24 16:19:53 +0530

    /Process.nextTick,Promise microtask & Timer

---
+++ W6/D2/P6.js
@@ -0,0 +1,15 @@
+//Process.nextTick,Promise microtask & Timer
+console.log("1.Start of Script");
+//Process.nextTick schedules a callback to run soon after the current syncronous code completes.
+process.nextTick(function(){
+    console.log("3.process.nextTick callback executed.");
+});
+//Promise microtask runs after nextTick in NodeJs
+Promise.resolve().then(function(){
+    console.log("4.Promise microtask executed.");
+});
+//callback timer runs later
+setTimeout(() => {
+    console.log("5. Timer callback executed.");
+},0);
+console.log("2.End of script");
