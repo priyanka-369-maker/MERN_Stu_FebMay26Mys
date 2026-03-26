30a495b3c75f9bf060d5bda81f45b68bde32be48
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-03-26 17:29:02 +0530

    //Inspecting request details in an HTTP server

---
+++ W6/D4/P3.js
@@ -0,0 +1,12 @@
+//Inspecting request details in an HTTP server
+const http = require("http");
+const server = http.createServer(function(req,res){
+    //writehead() sets the response status code and headers
+    res.writeHead(200,{"Content-Type":"text/plain"});
+    //end() send the response body and  closes the response
+    //req.method tells the http method,GET & POST
+    res.end("Method:"+req.method+"\nURL:"+req.url);
+});
+server.listen(3001,function(){
+    console.log("Server is running at http://localhost:3001");
+});
