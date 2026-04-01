4b130f4336ed343dbf2ed1bab3168bba71088eb7
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-01 10:33:22 +0530

    Handling asynchronous errors with callbacks

---
+++ W7/D3/P2.js
@@ -0,0 +1,26 @@
+//Handling asynchronous errors with callbacks
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+app.get("/file",function(req,res,next){
+    const filepath = path.join(__dirname,"newFile.txt");
+    fs.readFile(filepath,"utf8",function(error,data){
+        if(error){
+            //Forword the async error
+            return next(error);
+        }
+        res.send(data);
+    });
+});
+//centralized error handling middleware
+app.use(function(error,req,res,next){
+    res.status(404).json({
+        success:false,
+        message: "File/folder does not exist.",
+        originalMessage:error.message
+    });
+});
+app.listen(4000, function () {
+    console.log("Express server running at http://localhost:4000");
+});
