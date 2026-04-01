7a2c1c9d47e3544288b9fbcdf5b8ebab8175c869
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-01 13:00:29 +0530

    Generating token using login function and verifying the token

---
+++ W7/D3/P6.js
@@ -0,0 +1,32 @@
+//
+const jwt = require("jsonwebtoken");
+const secretKey = "monkey123";
+const wrongSecretKey = "mgn123";
+function loginUser(email,password){
+    if(email === "correct@email.com" && password === "mp123"){
+         const token = jwt.sign({
+            user:101,
+            email:email,
+            role:"student"
+         },secretKey,{expiresIn:"1h"});
+         return{
+            success:true,
+            token:token
+         };
+    }
+     return{
+            success:false,
+            message:"Invalid Credentials"
+        };
+}
+const loginResult = loginUser("correct@email.com","mp123");
+console.log("login result",loginResult);
+if(loginResult.success){
+    try{
+        const verifiedPayload = jwt.verify(loginResult.token,wrongSecretKey);
+        console.log("Verified Payload",verifiedPayload);
+    }
+    catch(error){
+        console.log("verification failed",error.message);
+    }
+}
