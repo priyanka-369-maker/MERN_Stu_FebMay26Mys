97a25e86d656abe0c4eeb09270a190b2d37c9c3e
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-02 14:45:56 +0530

    Setting and reading cookies

---
+++ W7/D4/P2.js
@@ -0,0 +1,22 @@
+//Setting and reading cookies
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
+//cookie-parser reads the cookie request header and places the parsed values into req.cookies
+app.use(cookieParser());
+app.get("/set-language", function(req,res){
+    res.cookie("language","english",{
+        maxAge:60*60*10000
+    });
+    res.send("Language cookies set to 'english' ")
+});
+app.get("/read-language", function(req,res){
+    const language = req.cookies.language;
+    res.json({
+        message:"cookies read from request.",
+        language:language || "No language cookie found"
+    });
+});
+app.listen(4000, function () {
+    console.log("JWT demo server running at http://localhost:4000");
+});
