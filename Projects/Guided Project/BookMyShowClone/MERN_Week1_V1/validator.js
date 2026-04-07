935b6cd6a940c83d0cbbc0d4d93284a095b697ea
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-07 11:07:10 +0530

    callback based validation functions

--- Projects/Guided Project/BookMyShowClone/MERN_Week1_V1/validator.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V1/validator.js
@@ -1,24 +1,35 @@
-//callback based validation functions
-function validateMovieSelection(movies,movieId,callback){
-const selectedMovie = movies.find((movie)=>movie.id === movieId);
-if (!selectedMovie){
-return callback("Invalid movie selection.choose a valid movie ID.",null);
+// Callback based validation functions
+
+function validateMovieSelection(movies,movieId,callback) {
+    const selectedMovie = movies.find((movie)=>movie.id === movieId);
+
+    if (!selectedMovie) {
+        return callback("Invalid movie selection.Choose a valid movie ID.",null);
+    }
+    callback(null,selectedMovie);
+
 }
-callback(null,selectedMovie);
+
+function validateTimeSelection(movie, selectedTime, callback) {
+  const selectedShowtime = movie.showtimes.find(
+    (show) => show.time.toLowerCase() === selectedTime.toLowerCase()
+  );
+
+  if (!selectedShowtime) {
+    return callback("Invalid time slot selection. Please choose a valid show time.", null);
+  }
+
+  callback(null, selectedShowtime);
 }
-function validateTimeSelection(movies,selectedTime,callback){
-const selectedShowTime = movies.showtimes.find((show)=>show.time.toLowerCase()===selectedTime.toLowerCase());
-if (!selectedShowTime){
-return callback("Invalid time slot selection.choose a valid time slot.",null);
+
+
+function validateSeatCount(seatCount,callback) {
+    if (isNaN(seatCount) || seatCount <= 0) {
+        return callback("Invalid seat count.Enter a valid seat count.",null);
+    }
+    callback(null,seatCount);
 }
-callback(null,selectedShowTime);
-}
-function validateSeatCount(seatCount,callback){
-if (!isNaN(seatCount) || seatCount <= 0){
-return callback("Invalid  seat count. Enter a valid time slot.",null);
-}
-callback(null,seatCount);
-}
+
 module.exports = {
     validateMovieSelection,
     validateTimeSelection,
