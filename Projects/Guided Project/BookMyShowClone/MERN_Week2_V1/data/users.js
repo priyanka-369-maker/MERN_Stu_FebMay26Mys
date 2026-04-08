1f8a18b38902fac768019d6d0b8752d310f9f8e3
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-08 10:07:19 +0530

    User's deatails of the application

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/users.js
@@ -0,0 +1,18 @@
+//User's deatails of the application
+const users = [
+    {
+        id:1,
+        name:"Priya",
+        email:"u@e.com",
+        password:"pri123",
+        role:"user"
+    },
+    {
+        id:2,
+        name:"Admin",
+        email:"a@e.com",
+        password:"admin123",
+        role:"admin"
+    }
+];
+module.exports=users;
