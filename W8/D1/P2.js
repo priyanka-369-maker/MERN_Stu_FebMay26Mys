a1585f6a74ff2e4bdf6224afaf9d30e1ed7fb371
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-11 10:43:51 +0530

    CRUD operations in MongoDB using Mongoose

--- W8/D1/P2.js
+++ W8/D1/P2.js
@@ -11,38 +11,39 @@ async function runCrudDemo(){
             role: String
         });
 
-        const Student = mongoose.models.Student || mongoose.model("Student",studentSchema);
+        const Student = mongoose.models.Department || mongoose.model("Student",studentSchema);
         //Clearing previous demo data 
         await Student.deleteMany({role:"demo-student"});
 
         //Create using save()
         const firstStudent = new Student({
-            name: "Priya",
+            name: "Rakesh",
             age: 20,
             role: "demo-student"
         });
         await firstStudent.save();
-        // console.log("Created new student with save()",firstStudent);
+        console.log("Created new student with save()",firstStudent);
 
         //Create using create()
         const secondStudent = await Student.create({
-            name: "geetha",
-            age: 21,
+            name: "Bipin",
+            age: 19,
             role: "demo-student"
         });
-        // console.log("Created new student with create()",secondStudent);
+        console.log("Created new student with create()",secondStudent);
 
         //Read using find()
-        const allDemoStudents = await Student.find({role:"demo-student"});
-        // console.log("Read with find(): ",allDemoStudents);
+        const allDemoStudents = await Student.find();
+        console.log("Read with find(): ",allDemoStudents);
 
         //Read using findOne()
-        const oneDemoStudent = await Student.findOne({name:"Geetha"});
+        const oneDemoStudent = await Student.findOne({name:"Bipin"});
         console.log("Read with findOne(): ",oneDemoStudent);
 
         //Update using findByIdAndUpdate()
+        const id = '69d882a41b1a446184ed04bf';
         const updatedStudent = await Student.findByIdAndUpdate(
-            secondStudent._id,
+            id,
             {age:20},
             {new:true}
         );
