c709643bc2e2b21c529c149cc93e215b92d2f7d5
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-11 10:44:32 +0530

    Applied filters to the query using comparision operators

--- W8/D1/P3.js
+++ W8/D1/P3.js
@@ -1,47 +1,66 @@
-//Applied filters to the query using comparision operators
+// Applied filters to the query using comparison operators
+// MERN_Stu_FebMay26Mys\W8\D1\P3.js
 const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+            name: String,
+            price: Number,
+            category: String,
+            status: String
+        }); 
+
+const Product = mongoose.models.Product || mongoose.model("Product",productSchema);
+
 async function runFilterDemo(){
     try{
+        
         await mongoose.connect("mongodb://localhost:27017/merntraining");
         console.log("MongoDB connected successfully");
-        const ProductSchema = new mongoose.Schema({
-                    name: String,
-                    price: Number,
-                    category: String,
-                    status:String
-                });
-                const Product = mongoose.models.Product || mongoose.model("Product",ProductSchema);
-                await Product.create([
-                {name: "laptop",
-                price: 50000,
-                category: "Electronic",
-                status: "active"},
-                {
-                name: "Bottle",
-                price: 500,
-                category: "Stationary",
-                status: "active"},
-                {
-                name: "choclate",
-                price: 50,
-                category: "Stationary",
-                status: "inactive"},
-                ]);
-                const equalQuery = await Product.find({status:{$eq:"active"}});
-                console.log("products which are active:",equalQuery);
-                //const greaterQuery = await Product.find({ price: { $gt: 5000 } });
-                //console.log("Products with price greater than 5000:", greaterQuery);
-                //const lesserQuery = await Product.find({ price: { $lt: 5000 } });
-                //console.log("Products with price lesser than 5000:", lesserQuery);
-                const notLaptopQuery = await Product.find({ name: { $ne: "laptop" } });
-                console.log("Products where name is not laptop:", notLaptopQuery);
 
-                await mongoose.connection.close();
-                console.log("connection closed");
+        await Product.create([
+            {name: "Book",
+            price: 300,
+            category: "Stationary",
+            status: "active"},
+            {name: "Pen",
+            price: 3000,
+            category: "Stationary",
+            status: "inactive"},
+            {name: "Laptop",
+            price: 60000,
+            category: "Electronics",
+            status: "active"},
+            {name: "CD",
+            price: 50,
+            category: "Electronics",
+            status: "inactive"}
+        ]);
 
+        // const productSchema = new mongoose.Schema({
+        //     name: String,
+        //     price: Number,
+        //     category: String,
+        //     status: String
+        // }); 
+
+        // const Product = mongoose.models.Product || mongoose.model("Product",productSchema);
+
+        const equalQuery = await Product.find({status:{$eq:"active"}});
+        // console.log("Products which are active:",equalQuery);
+
+        const greaterQuery = await Product.find({price:{$gt:5000}});
+        console.log("Products which are priced more than 5k:",greaterQuery);
+
+        const lesserQuery = await Product.find({price:{$lt:5000}});
+        console.log("Products which are priced less than 5k:",lesserQuery);
+
+        await mongoose.connection.close();
+        console.log("connection closed");
     }
     catch(error){
         console.log("Filter demo error:",error.message);
     }
 }
-runFilterDemo();
+// runFilterDemo();
+
+module.exports = Product;
