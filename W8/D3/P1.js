e8d487ed9f3c6767d1d73613503ee186af9d5d8d
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-14 11:41:50 +0530

    Validation and schema constraints

---
+++ W8/D3/P1.js
@@ -0,0 +1,45 @@
+
+//Validation and schema constraints
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    age: { type: Number, min: 18 },
+    role: {
+        type: String,
+        enum: ["admin", "user", "manager"]
+    },
+    email: {
+        type: String,
+        match: /.+@.+\..+/
+        // word@domainname.com .co.in/.org/.edu.in
+
+    }
+});
+const User = mongoose.model("HookValidationUser", userSchema);
+async function runValidationDemo() {
+    try {
+        const invalidUser = new User({
+            age: 15,
+            role: "guest",
+            email: "notvalidemail"
+        });
+        await invalidUser.validate();
+        // const validUser = new User({
+        //     age: 20,
+        //     role: "admin",
+        //     email: "p@s.com"
+        // });
+        // await validUser.validate();
+    }
+    catch (error) {
+        console.log("Validation errors found:");
+        for (const fieldName in error.errors) {
+            console.log(fieldName + ":" + error.errors[fieldName].message);
+        }
+    }
+}
+runValidationDemo();
+
