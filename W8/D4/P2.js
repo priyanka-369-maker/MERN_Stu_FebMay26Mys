98eb13cdbe54544345904df9b578e5eafeaa8680
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-16 10:35:35 +0530

    Timestamp and advanced queries

---
+++ W8/D4/P2.js
@@ -0,0 +1,40 @@
+//Timestamp and advanced queries
+const mongoose = require('mongoose');
+async function main(){
+    try{
+        await mongoose.connect('mongodb://127.0.0.1:27017/datedb');
+        console.log("Mongoose connected");
+        const demoSchema = new mongoose.Schema(
+            {
+                name:String,
+            },
+            {
+                timestamps:true
+            }
+        );
+         const Model = mongoose.model('LogTime',demoSchema);
+        //  await Model.deleteMany();
+        //   await Model.create([
+        //     { name: "moni" },
+        //     { name: "Arpi" },
+        //     { name: "gee" }
+        // ]);
+        const recent = await Model.find({
+            createdAt:{
+                $gte: new Date(Date.now()-1500000)
+            }
+        }).sort({ createdAt: -1 });
+        console.log("Recent:",recent);
+        
+
+        
+    }
+     catch(err){
+       console.error("Error:",err.message);
+    }
+    finally{
+                await mongoose.disconnect();
+                console.log("DB disconnected.");
+        }
+}
+main();
