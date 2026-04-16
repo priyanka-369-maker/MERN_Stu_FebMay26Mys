6a169674f97dc1ecaebdf2d41f729ec86faf5101
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-16 14:51:13 +0530

    Main file from where the project begins

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V2/events.js
+++
@@ -1,5 +0,0 @@
-// This file creates and exports a custom EventEmitter instance
-const EventEmitter = require("events");
-//Custom EventEmitter object
-const bookingEmitter = new EventEmitter();
-module.exports = bookingEmitter;
