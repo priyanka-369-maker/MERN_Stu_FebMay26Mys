00af7a1da83e8e0de53788c51a7e931b54ef2124
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-17 09:55:10 +0530

    Generateing OTP using Crypto and hashing it using bcrypt

---
+++ W8/D4/P7.js
@@ -0,0 +1,22 @@
+//Generateing OTP using Crypto and hashing it using bcrypt
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
+function generateOTP(length=6){
+    return crypto.randomInt(100000,999999).toString();
+}
+async function hashOTP(otp) {
+    return await bcrypt.hash(otp,10);
+
+    
+}
+async function verifyOTP(input,hash) {
+    return await bcrypt.compare(input,hash);
+
+}
+(async()=>{
+    const otp = generateOTP();
+    console.log("Generated OTP:",otp);
+    const hashedOTP = await hashOTP(otp);
+    console.log("Hashed OTP:",hashedOTP);
+    console.log("Verification (correct):",await verifyOTP(otp,hashedOTP));
+})();
