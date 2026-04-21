a6e19872cd431d5d104acdfa52deaf3ebb4a73ee
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-21 10:02:46 +0530

    Basic  app setup

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/server.js
@@ -0,0 +1,7 @@
+//Load environment variables
+require("dotenv").config();
+const app = require("./app");
+const PORT = process.env.PORT || 4000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port`,PORT);
+});
