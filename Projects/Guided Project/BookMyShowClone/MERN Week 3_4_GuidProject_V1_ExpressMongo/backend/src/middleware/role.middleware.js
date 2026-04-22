16fb1835715f5bd4f91707341a2338b0990bf7b3
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-22 09:56:22 +0530

    Created role middleware for RBAC

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/middleware/role.middleware.js
@@ -0,0 +1,13 @@
+//Role middleware: RBAC
+exports.authorize = (...roles)=>{
+    return (req,res,next)=>{
+        if(!req.user || !roles.includes(req.user.role)){
+             return res.status(403).json({
+            success:false,
+            message:"Access denied: insufficient permissions", 
+        });
+        }
+        next();
+
+    };
+};
