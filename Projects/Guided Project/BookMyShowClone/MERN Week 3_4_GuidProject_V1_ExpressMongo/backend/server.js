02251ba4cfb7bae6186968645427237cd418671d
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-23 09:03:44 +0530

    DB connection function invoked

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/server.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/server.js
@@ -5,7 +5,7 @@ const connectDB = require("./src/config/db");
 //Connect to database
 connectDB();
 //Port configuration
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`Server is running on port`,PORT);
 });
