d24cb62aa5fc60000f2fcc735d9625f47a140620
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-23 09:13:18 +0530

    Module for Managing User Profile Details

--- Team Project/LinkedIn Clone/MERN_Week1_V1.0/Profile.js
+++ Team Project/LinkedIn Clone/MERN_Week1_V1.0/Profile.js
@@ -1,18 +1,19 @@
 // Module for Managing User Profile Details
+const emitter = require("./events");
+
 function addSkill(user, skill) {
-    user.skills.push(skill);
+  user.skills.push(skill);
+  emitter.emit("profileUpdated");
 }
 
 function addExperience(user, exp) {
-    user.experience.push(exp);
+  user.experience.push(exp);
+  emitter.emit("profileUpdated");
 }
 
 function addEducation(user, edu) {
-    user.education.push(edu);
+  user.education.push(edu);
+  emitter.emit("profileUpdated");
 }
 
-module.exports = {
-    addSkill,
-    addExperience,
-    addEducation
-};
+module.exports = { addSkill, addExperience, addEducation };
