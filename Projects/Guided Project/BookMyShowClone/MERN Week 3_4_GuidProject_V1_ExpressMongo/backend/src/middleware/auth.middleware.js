bdf2d0fc90ae8f082b6ff123371c75719ad75c13
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-23 08:56:19 +0530

    Created Auth middleware in which verify token and fetched user data

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/middleware/auth.middleware.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/middleware/auth.middleware.js
@@ -1,41 +1,42 @@
 const jwt = require("jsonwebtoken");
-const user =require("../models/User");
+const User = require("../models/User");
+
 //Auth middleware
-exports.protect = async (req,res,next)=>{
- try{
-    let token;
-    if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
-        token = req.headers.authorization.spilt(" ")[1];
-    }
-    if(!token){
-        return res.status(404).json({
-            success:false,
-            message:"Not authorized, token missing", 
-        })
+exports.protect = async(req,res,next)=>{
+    try{
+        let token;
+        if(
+            req.headers.authorization && 
+            req.headers.authorization.startsWith("Bearer")
+        ){
+            token = req.headers.authorization.split(" ")[1];
+        }
+        if(!token){
+            return res.status(401).json({
+                success:false,
+                message:"Not authorized, token missing"
+            });
+        }
 
+        //verify token
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+
+        //Get user from DB
+        const user = await User.findById(decoded.id);
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:"User not found"
+            });
+        }
+        //Attach user to request
+        req.user = user;
+        next();
     }
-    //Verify token
-    const decoded = jwt.verify(token,process.env.JWT_SECRET);
-    
-    //Get user from DB
-    const user = await User.findById(decoded.id);
-    if(!user){
-         return res.status(401).json({
+    catch(error){
+        return res.status(401).json({
             success:false,
-            message:"User not found", 
+            message:"Invalid or expired token"
         });
-
     }
-    //Attach user to request
-    req.user = user;
-    next();
 }
-catch(error){
-    return res.status(401).json({
-            success:false,
-            message:"Invalid or expired token", 
-        }); 
-}
-
- };
-
