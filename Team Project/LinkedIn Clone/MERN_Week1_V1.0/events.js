49bbec2ccdba9e3c7f8b3f5cb3e03db1bf9c7d77
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-23 09:10:52 +0530

    Event Management Module using EventEmitter

--- Team Project/LinkedIn Clone/MERN_Week1_V1.0/events.js
+++ Team Project/LinkedIn Clone/MERN_Week1_V1.0/events.js
@@ -4,28 +4,16 @@ const chalk = require("chalk");
 
 const emitter = new EventEmitter();
 
-emitter.on("sessionStarted", () =>
-  console.log(chalk.blue("Session Started"))
-);
-
-emitter.on("profileCreated", (user) =>
-  console.log(chalk.green(`Profile created for ${user.name}`))
-);
-
-emitter.on("connectionRequested", () =>
-  console.log(chalk.cyan("Connection request sent"))
-);
-
-emitter.on("connectionAccepted", () =>
-  console.log(chalk.green("Connection accepted"))
-);
-
-emitter.on("postCreated", () =>
-  console.log(chalk.green("Post created successfully"))
-);
-
-emitter.on("operationFailed", (msg) =>
-  console.log(chalk.red(msg))
-);
+emitter.on("sessionStarted", () => console.log(chalk.blue("Session Started")));
+emitter.on("profileCreated", u => console.log(chalk.green(`Profile created: ${u.name}`)));
+emitter.on("profileUpdated", () => console.log(chalk.yellow("Profile updated")));
+emitter.on("connectionRequested", () => console.log(chalk.cyan("Request sent")));
+emitter.on("connectionAccepted", () => console.log(chalk.green("Request accepted")));
+emitter.on("connectionRejected", () => console.log(chalk.red("Request rejected")));
+emitter.on("postCreated", () => console.log(chalk.green("Post created")));
+emitter.on("postLiked", () => console.log(chalk.blue("Post liked")));
+emitter.on("commentAdded", () => console.log(chalk.blue("Comment added")));
+emitter.on("feedViewed", () => console.log(chalk.cyan("Feed viewed")));
+emitter.on("operationFailed", msg => console.log(chalk.red(msg)));
 
 module.exports = emitter;
