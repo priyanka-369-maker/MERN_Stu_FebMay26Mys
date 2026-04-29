85bf0c12a08a37a448d72a8921fdf8432b8e6f53
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-29 14:46:59 +0530

    Prop validation

---
+++ W9/D2/prop-example-app/src/components/P11.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// Component
+function Profile({ name, age }) {
+  return (
+    <div className="card">
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
+    </div>
+  );
+}
+
+// Prop validation
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+};
+
+// Parent component
+export function PropTypesDemo() {
+  return (
+    <>
+      <Profile name="Priya" age={21} />
+    </>
+  );
+}
