9272c92c682ca36528b768c6975eea20c8d422a2
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-29 10:52:30 +0530

    React application to learn  Components concepts

--- W9/D2/my-app/src/components/FunctionalComponentsAdv.jsx
+++ W9/D2/my-app/src/components/FunctionalComponentsAdv.jsx
@@ -1,43 +1,23 @@
-import {React} from "react";
-//List rendering
+// src\FunctionalComponentsAdv.jsx
+import { React } from "react";
+// List rendering
 const users = [
-    {id:1, role:"Student"},
-    {id:2, role:"Trainer"},
-    {id:3, role:"Admin"},
-    {id:4, role:"Developer"},
+    { id:1, role: "Student"},
+    { id:2, role: "Trainer"},
+    { id:3, role: "Admin"},
+    { id:4, role: "Developer"}
 ];
-export function FunctionalComp(){
+
+export function FunctionComp(){
     return(
         <div>
             <h2>Functional component</h2>
             {/* List rendering */}
             {users.map((user)=>(
-                <p key={user.id}>
-                    {user.role}
+                <p key={user.id} className="pclass">
+                   {user.role} 
                 </p>
             ))}
         </div>
     );
 }
-// import React from "react";
-
-// const users = [
-//     { id: 1, role: "Student" },
-//     { id: 2, role: "Trainer" },
-//     { id: 3, role: "Admin" },
-//     { id: 4, role: "Developer" },
-// ];
-
-// export function FunctionalComp() {
-//     return (
-//         <div className="container">
-//             <h2 data-type="heading">Functional component</h2>
-
-//             {users.map((user) => (
-//                 <p key={user.id} className="user-text">
-//                     {user.role}
-//                 </p>
-//             ))}
-//         </div>
-//     );
-// }
