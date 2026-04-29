a537040b6ef93cbeffb652638d64c71af5221c03
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-29 10:47:13 +0530

    Created React app via CRA approach

--- W9/D1/my-app/src/App.js
+++ W9/D1/my-app/src/App.js
@@ -22,10 +22,15 @@ import './App.css';
 //   );
 // }
  function App(){
+  const user = "Priya";
+  const marks = 65;
             return(
                 <div>
-                    <h1>React Introduction</h1>
-                    <p> React simplifies UI creation through reusable components.</p>
+                  {/* Expressions within JSX */}
+                  {/* { curly brackets }: in JSX means:
+                  switch from JSX/HTML-like syntax into JavaScript */}
+                    <h1>Hello,{user}</h1>
+                    <p>Updated marks: {marks+10}</p>
                 </div>
             );
         }
