bd6b652b2d014a38514543ceb6abb04354f98c9f
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-04-29 14:10:48 +0530

    Props vs state

---
+++ W9/D2/prop-example-app/src/components/P9.jsx
@@ -0,0 +1,27 @@
+
+// Props vs state
+// Props: data passed from parent to chlid component
+//data comes from outside the component are read-only are used for passing data between components
+// State: data managed inside a component
+//belongs to the component itself
+// can be modified/changed
+
+import { useState } from "react"
+
+//setter method is used to modify data
+function Child({title}) {
+    return <p>Props: {title}</p>
+}
+export function PropsState(){
+    const [stateValue, setStateValue] = useState('Local State');
+    return(
+        <>
+        <h3>Props vs State</h3>
+        <Child title ="Parent data" />
+        <p>State: {stateValue}</p>
+       <button onClick={()=>setStateValue('This is new State')}>
+        Update State
+        </button> 
+        </>
+    )
+}
