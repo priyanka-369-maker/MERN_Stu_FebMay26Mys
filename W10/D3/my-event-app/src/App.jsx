4d46c7a498942c00819bac316cf372f078971771
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-05-08 09:18:38 +0530

    React events

---
+++ W10/D3/my-event-app/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import { EventBasics } from "./components/P1";
+import { EventObject } from "./components/P2";
+import { PassingArguments } from "./components/P3";
+import { UseCallbackEvents } from "./components/P5";
+import { EventDelegation } from "./components/P6";
+import { FormsPattern } from "./components/P7";
+import { KeyboardEvent } from "./components/P8";
+import { EventObj } from "./components/P9";
+
+function App() {
+   return (
+    <>
+      {/* <EventBasics /> */}
+      {/* <EventObject /> */}
+      {/* <PassingArguments /> */}
+      {/* <UseCallbackEvents /> */}
+      {/* <EventDelegation /> */}
+      {/* <FormsPattern /> */}
+      {/* <KeyboardEvent /> */}
+      <EventObj />
+    </>
+  )
+}
+export default App
