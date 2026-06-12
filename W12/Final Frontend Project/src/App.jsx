431a95298dc25c776bcc089b21730e57a76ff578
priyanka-369-maker <priyashivaram2005@gmail.com>
2026-06-12 12:04:52 +0530

    Usage of Props

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -2,12 +2,36 @@
 
 import './App.css'
 // import MovieCard from "./components/MovieCard";
-import HomePage from "./pages/HomePage";
+// import HomePage from "./pages/HomePage";
+import MovieCard from "./components/MovieCard";
 function App() {
  
 
   return (
-    <HomePage/>
+    <>
+    <MovieCard
+        title="Inception"
+        genre="Sci-Fi"
+        rating={4.8}
+        duration="2h 28m"
+        poster="https://picsum.photos/200/300?1"
+        />,
+          <MovieCard
+        title="Avatar"
+        genre="Sci-Fi"
+        rating={4.1}
+        duration="3h 28m"
+        poster="https://picsum.photos/200/300?2"
+        />,
+          <MovieCard
+        title="FightClub"
+        genre="Sci-Fi"
+        rating={4.9}
+        duration="2h 28m"
+        poster="https://picsum.photos/200/300?3"
+        />
+        </>
+    // <HomePage/>
     //   <div>
 
     //         <h1>
